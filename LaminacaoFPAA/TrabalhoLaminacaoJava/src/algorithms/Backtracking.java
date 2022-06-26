eb43d481a6396649da10264be747e92d73094825
Edson Júnior Campolina Silva <ejcsengsoft@gmail.com>
2022-06-26 12:54:07 -0300

    Add files via upload

--- LaminacaoFPAA/TrabalhoLaminacaoJava/src/algorithms/Backtracking.java
+++ LaminacaoFPAA/TrabalhoLaminacaoJava/src/algorithms/Backtracking.java
@@ -3,39 +3,63 @@ package algorithms;
 import java.util.List;
 
 import entities.Lamina;
+import entities.LeitorArquivo;
 
 public class Backtracking {
 
-	public static int backtrackingRecursivo(int tamanho, int reducao, List<Lamina> lista) {
+	public static void backtracking(String path, List<String> log) {
+		List<Lamina> listaLamina = LeitorArquivo.leituraArquivoLista(path);
+		log.add("Procurando valor via BackTracking");
+		int c1 = Backtracking.backtrackingRecursivo(listaLamina.get(0).getLargura(),
+				listaLamina.get(0).getCustoReducao3(), 3, listaLamina);
 
-		int c1 = 0;
-		int c2 = 0;
-		int c3 = 0;
-		int tam = tamanho - reducao;
-		if (tam > 6) {
-			c3 += backtrackingRecursivo(tam, 3, lista);
-			c2 += backtrackingRecursivo(tam, 2, lista);
-			c1 += backtrackingRecursivo(tam, 1, lista);
-		} else if (tam == 6) {
-			c2 += backtrackingRecursivo(tam, 2, lista);
-			c1 += backtrackingRecursivo(tam, 1, lista);
-		} else if (tam == 5) {
-			c1 += backtrackingRecursivo(tam, 1, lista);
-		} else if (tam == 4) {
-			if (reducao == 3) {
-				return Lamina.encontraLaminaPorLargura(lista, tam + 3).getCustoReducao3();
-			} else if (reducao == 2) {
-				return Lamina.encontraLaminaPorLargura(lista, tam + 2).getCustoReducao2();
-			} else if (reducao == 1) {
-				return Lamina.encontraLaminaPorLargura(lista, tam + 1).getCustoReducao1();
-			}
+		int c2 = Backtracking.backtrackingRecursivo(listaLamina.get(0).getLargura(),
+				listaLamina.get(0).getCustoReducao2(), 2, listaLamina);
+
+		int c3 = Backtracking.backtrackingRecursivo(listaLamina.get(0).getLargura(),
+				listaLamina.get(0).getCustoReducao1(), 1, listaLamina);
+		if (c3 <= c2 && c3 <= c1) {
+			log.add("Melhor valor encontrado via backtracking: " + c3);
+		} else if (c2 <= c1) {
+			log.add("Melhor valor encontrado via backtracking: " + c2);
+		} else {
+			log.add("Melhor valor encontrado via backtracking: " + c1);
 		}
-		if (c3 != 0 && c3 <= c2 && c3 <= c1) {
-			return c3;
-		} else if (c2 != 0 && c2 <= c1) {
-			return c2;
+		log.add("");
+		log.add("");
+		log.add("");
+	}
+
+	// Este método poderia ser private, entretanto daria erro nos prgramas de teste
+	public static int backtrackingRecursivo(int tamanho, int custo, int reducao, List<Lamina> lista) {
+		int custo1 = custo;
+		int custo2 = custo;
+		int custo3 = custo;
+		int novoTamanho = tamanho - reducao;
+		Lamina lamina = new Lamina();
+		if (novoTamanho > 4) {
+			lamina = Lamina.encontraLaminaPorLargura(lista, novoTamanho);
 		}
-		return c1;
+		if (novoTamanho > 6) {
+			custo3 = backtrackingRecursivo(novoTamanho, lamina.getCustoReducao3() + custo, 3, lista);
+			custo2 = backtrackingRecursivo(novoTamanho, lamina.getCustoReducao2() + custo, 2, lista);
+			custo1 = backtrackingRecursivo(novoTamanho, lamina.getCustoReducao1() + custo, 1, lista);
+			if (custo3 <= custo2 && custo3 <= custo1)
+				return custo3;
+			else if (custo2 <= custo1)
+				return custo2;
+			return custo1;
+		} else if (novoTamanho == 6) {
+			custo2 = backtrackingRecursivo(novoTamanho, lamina.getCustoReducao2() + custo, 2, lista);
+			custo1 = backtrackingRecursivo(novoTamanho, lamina.getCustoReducao1() + custo, 1, lista);
+			if (custo2 <= custo1)
+				return custo2;
+			return custo1;
+		} else if (novoTamanho == 5) {
+			custo1 = backtrackingRecursivo(novoTamanho, lamina.getCustoReducao1() + custo, 1, lista);
+			return custo1;
+		}
+		return custo;
 
 	}
 
